cba6349f343f057919776de1d290f7983ef7fd0e
Farda Alan M <fardaalan@dinustek.com>
2021-01-17 03:13:31 +0700

    uas pemweb

---
+++ index.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>YUKIKA</title>
+
+    <link rel="stylesheet" href="asset/css/bootstrap.min.css">
+    <link rel="stylesheet" href="asset/css/style.css">
+    <link rel="stylesheet" href="asset/css/all.min.css">
+    <link rel="stylesheet" href="asset/css/fontawesome.min.css">
+
+    <script src="asset/js/jquery.min.js"></script>
+    <script src="asset/js/bootstrap.js"></script>
+    <script src="asset/js/bootstrap.min.js"></script>
+    
+</head>
+<body>
+    
+    <section>
+        <nav class="navbar navbar-expand-lg navbar-light bg-satu">
+            <a class="text-info navbar-brand logo" href="#">YUKIKA</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+                </ul>
+                <form class="form-inline my-2 my-lg-0">
+                    <a href="#" class="btn btn-md btn-info">Login</a> &nbsp
+                    <a href="#" class="btn btn-md btn-outline-info">Register</a>
+                </form>
+            </div>
+        </nav>
+    </section>
+
+    <section>
+        <div class="container-fluid text-white text-center pt-5 pb-5 bg-dua">
+            <div class="logo">
+                <h3>YUKIKA</h3>
+            </div>
+            <p>Dengan <span class="logo">YUKIKA</span> kamu bisa mencari pekerjaan yang sesuai dengan keinginan kamu, tunggu apalagi ? <b>Yuk kita kerja !</b></p>
+        </div>
+    </section>
+
+    <section>
+        <div class="container mt-3 mb-3 text-dark">
+            <div class="row">
+                <div class="col-lg-6 text-left">
+                    <h4>Menu</h4>
+                </div>
+                <div class="col-lg-6 text-right">
+                    <a href="#" class="text-info btn-a">Lihat Semua</a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="card">
+                        <div class="card-body text-center">
+                            <i class="fas fa-school" style="font-size: 40px"></i>
+                            <hr>
+                            <h5>Pelatihan</h5>
+                        </div>
+                        <div class="card-footer bg-info text-center">
+                            <a href="#" class="btn btn-md btn-outline-light">Lihat Pelatihan</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4">
+                    <div class="card">
+                        <div class="card-body text-center">
+                            <i class="fas fa-building" style="font-size: 40px"></i>
+                            <hr>
+                            <h5>Lowongan</h5>
+                        </div>
+                        <div class="card-footer bg-info text-center">
+                            <a href="#" class="btn btn-md btn-outline-light">Lihat Pelatihan</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4">
+                    <div class="card">
+                        <div class="card-body text-center">
+                            <i class="fas fa-user" style="font-size: 40px"></i>
+                            <hr>
+                            <h5>Jasa</h5>
+                        </div>
+                        <div class="card-footer bg-info text-center">
+                            <a href="#" class="btn btn-md btn-outline-light">Lihat Pelatihan</a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            
+        </div>
+    </section>
+
+    
+    <section>
+        <div class="container-fluid bg-dua text-white pt-4 pb-4">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <h6>YUKIKA</h6>
+                        <h6>YUK KITA KERJA</h6>
+                        <br>
+                        <p>Jl. Imam Bonjol No.207, Pendrikan Kidul, Kec. Semarang Tengah, Kota Semarang, Jawa Tengah 50131</p>
+
+                    </div>
+
+                    <div class="col-lg-4">
+                        <p>
+                            Facebook    : YUKIKA <br>
+                            Instagram   : @yukika <br>
+                            Email       : yukika@gmail.com <br>
+                            Website     : fardaalan.xyz
+                        </p>
+                    </div>
+
+                    <div class="col-lg-4 text-center">
+                        <h5>YUK KITA KERJA !</h5>
+                        <i class="fas fa-user-tie text-white" style="font-size: 80px"></i>
+                        <h4>YUKIKA</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <footer>
+            <div class="container-fluid bg-satu text-center pt-3 pb-3 text-dark">
+                &copy 2020. Coffee Developer. All Right Reserved
+            </div>
+        </footer>
+    </section>
+
+</body>
+</html>
